734873bfbd6b6e54fbaec344d7a887ee438ab128
dadakhon09 <odadaxon27@mail.ru>
2019-07-28 17:28:13 +0500

    sendmail done

--- mail/serializers.py
+++ mail/serializers.py
@@ -1,4 +1,9 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 from rest_framework import serializers
+from django.conf import settings
+from django.core.mail import EmailMessage
+
 from mail.models import Startup
 
 class StartupListSerializer(serializers.ModelSerializer):
@@ -15,6 +20,13 @@ class StartupCreateSerializer(serializers.ModelSerializer):
 		s = Startup.objects.create(**validated_data)
 		file = self.context['request'].FILES.get('file')
 		s.file = file
-		print(self.context['request'].FILES)
 		s.save()
+		print(s.file)
+		from_email = settings.EMAIL_HOST_USER
+		recipient_list = ['odadaxon99@gmail.com',]
+		subject = f'New Startup idea from {s.sender_name}'
+		message = f' Title: {s.title} \n Sender email: {s.sender_email} \n Content: {s.content} \n'
+		msg = EmailMessage(subject, message, from_email, recipient_list)
+		msg.attach_file(os.path.join(BASE_DIR, f'{s.file}'))
+		msg.send()
 		return s
--- mail/views.py
+++ mail/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from django.core.mail import send_mail
 from django.conf import settings
 from django.http import HttpResponse
+from django.views import View
 
 from mail.serializers import StartupCreateSerializer, StartupListSerializer
 from mail.models import Startup
@@ -10,13 +11,18 @@ from mail.models import Startup
 def home(request):
 	return render(request, 'home.html')
 
-def sendmail(request):
-	subject = 'test title'
-	message = 'something new'
-	from_email = settings.EMAIL_HOST_USER
-	recipient_list = ['ortikovaadiba13@gmail.com','odadaxon27@mail.ru']
-	send_mail(subject, message, from_email, recipient_list, fail_silently=False)
-	return HttpResponse('Your message has been successfully sent')
+# class Sendmail(View):
+# 	def post(self, request):
+# 		print(request)
+# 		return HttpResponse('Your message has been successfully sent')
+
+# 	def get(self, request):
+# 		subject = 'test title'
+# 		message = 'something new'
+# 		from_email = settings.EMAIL_HOST_USER
+# 		recipient_list = ['odadaxon99@gmail.com','odadaxon27@mail.ru']
+# 		send_mail(subject, message, from_email, recipient_list, fail_silently=False)
+# 		return HttpResponse('Your message has been successfully sent')
 
 class StartupCreateAPIView(generics.CreateAPIView):
 	serializer_class = StartupCreateSerializer
